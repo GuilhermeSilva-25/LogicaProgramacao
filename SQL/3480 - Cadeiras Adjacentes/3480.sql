256adc06ad54d19649ad4637106a26b5e163bab9
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-12-04 20:07:38 -0300

    feat: add solutions for problem 3480 - Cadeiras Adjacentes

---
+++ SQL/3480 - Cadeiras Adjacentes/3480.sql
@@ -0,0 +1,62 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 3480
+
+CREATE TABLE chairs (
+    id numeric PRIMARY KEY,
+    queue numeric,
+    available boolean
+);
+
+INSERT INTO chairs (id, queue, available) VALUES
+(1, 1, FALSE),
+(2, 1, FALSE),
+(3, 1, TRUE),
+(4, 1, FALSE),
+(5, 1, FALSE),
+(6, 1, FALSE),
+(7, 1, TRUE),
+(8, 1, TRUE),
+(9, 2, TRUE),
+(10, 2, FALSE),
+(11, 2, TRUE),
+(12, 2, TRUE),
+(13, 2, FALSE),
+(14, 2, TRUE),
+(15, 2, TRUE),
+(16, 2, FALSE),
+(17, 3, TRUE),
+(18, 3, FALSE),
+(19, 3, TRUE),
+(20, 3, FALSE),
+(21, 3, TRUE),
+(22, 3, TRUE),
+(23, 3, FALSE),
+(24, 3, FALSE),
+(25, 4, TRUE),
+(26, 4, FALSE),
+(27, 4, FALSE),
+(28, 4, TRUE),
+(29, 4, TRUE),
+(30, 4, FALSE),
+(31, 4, FALSE),
+(32, 4, TRUE);
+
+/*  Execute this query to drop the tables */
+-- DROP TABLE chairs;
+
+
+SELECT 
+    c1.queue,
+    c1.id AS left_seat,
+    c2.id AS right_seat
+FROM chairs c1
+JOIN chairs c2
+    ON c1.queue = c2.queue
+   AND c2.id = c1.id + 1
+WHERE 
+    c1.available = TRUE
+    AND c2.available = TRUE
+ORDER BY 
+    left_seat;
